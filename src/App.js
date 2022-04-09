3e11cc5d6d9d351a31ee245ec11fb028abdf7cb7
ArushaK <arusha.kamate007@gmail.com>
2022-04-09 16:05:51 +0530

    Input done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Card from './UI/Card'
+import Input from './UI/Input'
+import Modal from './UI/Modal'
 
 function App() {
   return (
     <div className="App">
-     
+     <Card>
+       <Input label="Name"
+        input={{
+          id: "name",
+          type: "text",
+          defaultValue: " ",
+        }}/>
+   
+       </Card>
     </div>
   );
 }
